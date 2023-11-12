996aa7616e7cf3d17cb043e1152af88f9631d283
ItsDrike <itsdrike@protonmail.com>
2023-11-12 23:02:19 +0100

    Add wakatime

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -139,4 +139,22 @@ return {
       end
     end,
   },
+
+  -- Code time & habit tracking
+  {
+    "wakatime/vim-wakatime",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = {
+      "WakaTimeApiKey",
+      "WakaTimeDebugEnable",
+      "WakaTimeDebugDisable",
+      "WakaTimeScreenRedrawEnable",
+      "WakaTimeScreenRedrawEnableAuto",
+      "WakaTimeScreenRedrawDisable",
+      "WakaTimeToday",
+      "WakaTimeCliLocation",
+      "WakaTimeCliVersion",
+      "WakaTimeFileExpert",
+    },
+  },
 }
