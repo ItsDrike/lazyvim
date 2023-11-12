bd94d84e2c843b69ab474517c3ed4a3911c7db86
ItsDrike <itsdrike@protonmail.com>
2023-11-12 23:02:13 +0100

    Replace mini.pairs wiht nvim-autopairs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -106,4 +106,37 @@ return {
       opts.mapping["<CR>"] = cmp.mapping.confirm({ select = false })
     end,
   },
+
+  -- Disable mini.pairs in favor of nvim-autopairs
+  {
+    "mini.pairs",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "User CustomFile",
+    opts = {
+      check_ts = true,
+      ts_config = { java = false },
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0,
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr",
+      },
+    },
+    config = function(_, opts)
+      local npairs = require("nvim-autopairs")
+      npairs.setup(opts)
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if cmp_status_ok then
+        cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done({ tex = false }))
+      end
+    end,
+  },
 }
