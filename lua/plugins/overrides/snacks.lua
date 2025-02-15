07a1604489e12ef80dfe075b7bcdcf82aff4e0d1
ItsDrike <itsdrike@protonmail.com>
2025-02-15 20:49:20 +0100

    Use a faster animation for scrolling

---
+++ lua/plugins/overrides/snacks.lua
@@ -0,0 +1,21 @@
+---@type LazySpec
+return {
+  "folke/snacks.nvim",
+  ---@module 'snacks'
+  ---@type snacks.Config
+  opts = {
+    -- Faster scroll animation
+    scroll = {
+      animate = {
+        duration = { step = 15, total = 150 },
+        easing = "inOutCubic",
+      },
+      -- faster animation when repeating scroll after delay
+      animate_repeat = {
+        delay = 500, -- delay in ms before using the repeat animation
+        duration = { step = 5, total = 50 },
+        easing = "linear",
+      },
+    },
+  },
+}
